7e9e7eacf3561b4716a7013e6eb3fd001e09c914
fmach24 <147771915+fmach24@users.noreply.github.com>
2025-05-12 12:50:55 +0200

    Delete zadanie_1.py

--- zadanie_1.py
+++
@@ -1,8 +0,0 @@
-# ZADANIE 1: Stwórz funkcję tak, by poprawnie działała jako coroutine i czekała 1 sekundę.
-
-import asyncio
-
-# ← tutaj uzupełnij
-    print("Po 1 sekundzie")
-
-asyncio.run(moja_funkcja())
